bfead0d65bac50e75e9df182077acc0a6993ccd2
Chris Michael <chris.perez@upr.edu>
2019-10-30 23:29:30 -0400

    search url defined.

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,5 +1,6 @@
 module.exports = {
   BASE_URL: 'https://animeflv.net',
   BROWSE_URL: 'https://animeflv.net/browse?',
+  SEARCH_URL: 'https://animeflv.net/browse?q=',
   ANIME_VIDEO_URL: 'https://animeflv.net/ver/'
 };
