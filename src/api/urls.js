1794378535f9346bc9d3dadd4d8d35f0a3c61881
Chris Michael <chris.perez@upr.edu>
2019-11-23 10:59:57 -0400

    BASE_EPISODE_IMG_URL added as url

--- src/api/urls.js
+++ src/api/urls.js
@@ -2,5 +2,6 @@ module.exports = {
   BASE_URL: 'https://animeflv.net',
   BROWSE_URL: 'https://animeflv.net/browse?',
   SEARCH_URL: 'https://animeflv.net/browse?q=',
-  ANIME_VIDEO_URL: 'https://animeflv.net/ver/'
+  ANIME_VIDEO_URL: 'https://animeflv.net/ver/',
+  BASE_EPISODE_IMG_URL: 'https://cdn.animeflv.net/screenshots/'
 };
