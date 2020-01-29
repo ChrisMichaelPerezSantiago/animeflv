8e720c2c2e0f17e6b27d598df2ef1ff74d3fb46d
Chris Michael <chris.perez@upr.edu>
2020-01-29 17:49:36 -0400

    [BASE_JIKA_URL , BASE_MYANIME_LIST_URL] urls defined.

--- src/api/urls.js
+++ src/api/urls.js
@@ -3,5 +3,7 @@ module.exports = {
   BROWSE_URL: 'https://animeflv.net/browse?',
   SEARCH_URL: 'https://animeflv.net/browse?q=',
   ANIME_VIDEO_URL: 'https://animeflv.net/ver/',
-  BASE_EPISODE_IMG_URL: 'https://cdn.animeflv.net/screenshots/'
+  BASE_EPISODE_IMG_URL: 'https://cdn.animeflv.net/screenshots/',
+  BASE_JIKA_URL: ' https://api.jikan.moe/v3/search/anime?q=',
+  BASE_MYANIME_LIST_URL : 'https://myanimelist.net/character/'
 };
